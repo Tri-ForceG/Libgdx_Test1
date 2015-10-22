37d9e3a1edeff27fff39d871226a4ae5f72fe885
Abraham-7 <noria0540@hotmail.com>
2015-10-22 09:53:05 -0400

    Change to white

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -18,7 +18,7 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 7, 4, 6);
+		Gdx.gl.glClearColor(0, 1, 1, 6);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
